b9bf275fc970822d72b00cc6bdc3b8ad3b670c55
Yisus2002 <150860957+Yisus2002@users.noreply.github.com>
2023-11-29 13:28:08 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,37 @@
+function realizarAnimacion1() {
+    var foto = document.getElementById('fotopython');
+    var texto = document.getElementById('textopython');
+
+    // Mostrar el texto después de un breve retraso
+    setTimeout(function() {
+        texto.style.opacity = 1;
+    }, 500); // 500 milisegundos (0.5 segundos)
+}
+
+function realizarAnimacion2(){
+    var foto = document.getElementById('fotojava');
+    var texto = document.getElementById('textojava');
+
+    // Mostrar el texto después de un breve retraso
+    setTimeout(function() {
+        texto.style.opacity = 1;
+    }, 500); // 500 milisegundos (0.5 segundos)
+}
+function realizarAnimacion3(){
+    var foto = document.getElementById('fotoc++');
+    var texto = document.getElementById('textoc++');
+
+    // Mostrar el texto después de un breve retraso
+    setTimeout(function() {
+        texto.style.opacity = 1;
+    }, 500); // 500 milisegundos (0.5 segundos)
+}
+function realizarAnimacion4(){
+    var foto = document.getElementById('fotophp');
+    var texto = document.getElementById('textophp');
+
+    // Mostrar el texto después de un breve retraso
+    setTimeout(function() {
+        texto.style.opacity = 1;
+    }, 500); // 500 milisegundos (0.5 segundos)
+}
